242551a148d993a66e49351c7d86af62112958ee
bryan <bmugenya@gmail.com>
2017-10-31 08:41:34 +0300

    CSC 314, first commit

---
+++ learning_logs/static/learning_logs/js/init.js
@@ -0,0 +1,24 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+    $().ready(function(){
+        $('[rel="tooltip"]').tooltip();
+
+    });
+
+    function rotateCard(btn){
+        var $card = $(btn).closest('.card-container');
+        console.log($card);
+        if($card.hasClass('hover')){
+            $card.removeClass('hover');
+        } else {
+            $card.addClass('hover');
+        }
+    }
